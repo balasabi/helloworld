8ac30c630dbe1851a0f40a764c7245aad6782120
balasabi <balas@kenlasystems.com>
2021-09-24 20:48:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var app = express()
+
+//Define request response in root URL (/)
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+//Launch listening server on port 8081
+app.listen(3000, function () {
+  console.log('app listening on port 3000!')
+})
